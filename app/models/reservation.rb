2714d405e2a4594c0f93f45e669cc4796b816ed9
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-06-08 19:04:45 +0500

    Added validation to models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,7 @@
 class Reservation < ApplicationRecord
+  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :car, foreign_key: 'car_id'
+
+  validates :date, presence: true
+  validates :city, presence: true
 end
