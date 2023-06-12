d11936c7bc4ef25aec51e5c2c6e612f5dfe83c6b
Ogaga01 <iyaraogaga69@gmail.com>
2023-06-12 07:59:03 +0100

    Add car controller spec

---
+++ spec/controllers/car_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe CarsController, type: :controller do
+  before :all do
+    @user = User.create(name: 'Testing', username: 'testing')
+    @car = Car.create(name: 'car', model: 'BMW', description: 'A BMW car', image: 'picture link', price: 250, user_id: @user.id)
+  end
+
+  describe 'POST create' do
+    it 'returns status 201 (created)' do
+      post :create,
+           params: { name: 'car', model: 'BMW', description: 'A BMW car', image: 'picture link', price: 250, user_id: @user.id }
+      expect(response.status).to eq(201)
+    end
+
+    it 'returns status 500 (internal_server_error) ' do
+      post :create,
+           params: { name: 'car', model: 'BMW', description: 'A BMW car', image: 'picture link', price: -250, user_id: @user.id }
+      expect(response.status).to eq(500)
+    end
+  end
+
+  describe 'GET index' do
+    it 'returns cars array' do
+      get :index
+      expect(response.status).to eq(200)
+      parsed_response = JSON.parse(response.body)
+      expect(parsed_response).not_to be_empty
+    end
+  end
+end
