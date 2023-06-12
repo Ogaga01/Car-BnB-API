59be894818980c03ecf9cfbac032e26aff10d55b
Ogaga Iyara <iyaraogaga69@gmail.com>
2023-06-12 06:58:18 +0100

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 
-  get 'user/login', to: 'users#login'
-  get 'user/register', to: 'users#register'
-  resources :users, only: %i[new] do
-    get 'cars/reserve', to: 'cars#reserve'
-    get 'cars', to: 'cars#user_cars'
-    resources :cars, except: %i[edit new] do
-      resources :reservations, only: %i[index create]
-    end
-  end
+  # Login and register paths
+  post 'api/v1/register', to: 'users#register'
+  get 'api/v1/login/:username', to: 'users#login'
+
+  # Car endpoints
+  get 'api/v1/cars', to: 'cars#index'
+  get 'api/v1/car/:id', to: 'cars#show'
+  post 'api/v1/car', to: 'cars#create'
+  delete 'api/v1/car/:id', to: 'cars#delete'
+  get 'api/v1/reserve/cars/:date', to: 'cars#reserve'
+  get 'api/v1/cars/:id', to: 'cars#user_cars' 
+
+  # Reservation endpoints
+  post 'api/v1/reservation', to: 'reservations#create'
+  get 'api/v1/reservations', to: 'reservations#index'
   
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
